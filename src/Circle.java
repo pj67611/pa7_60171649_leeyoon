798361a6d70d07863c72b0215e87cb386266db83
unknown <pj67611@naver.com>
2017-11-20 02:40:41 +0900

    pa7 commit

---
+++ src/Circle.java
@@ -0,0 +1,30 @@
+//60171649 이윤
+public class Circle extends Shape {
+	int x,y,radius;                                 //원의 중점(x,y)좌표와 반지름을 저장할 변수 생성
+	double Area;                                    //원의 넓이를 저장할 변수 생성.
+	double Length;                                  //원의 넓이를 저장할 변수 생성.
+	
+	public Circle() {                               //기본 생성자 만들어줌
+	}
+
+	public Circle(int x,int y,int radius) {         //Circle인스턴스를 생성할때 argument를 parameter로 받는다.
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.Area = 3.14*radius*radius;
+		this.Length = 2*3.14*radius;
+	}
+	
+	public double getArea() {
+		return Area;
+	}
+
+	public double getLength() {
+		return Length;
+	}
+	
+	public void draw() {
+		System.out.printf("Circle center(%d,%d)-radius(%d)\n",x,y,radius);
+	}
+
+}
---
+++ src/Shape.java
@@ -0,0 +1,9 @@
+//60171649 이윤
+public abstract class Shape implements Drawable{
+	public abstract void draw();                 //하위 클래스에서 draw 메소드의 구체적인 로직을 만들도록 강제함.
+
+	public abstract double getArea();            //하위 클래스에서 getArea 메소드의 구체적인 로직을 만들도록 강제함.
+	
+	public abstract double getLength();          //하위 클래스에서 getLength 메소드의 구체적인 로직을 만들도록 강제함.
+ 
+}
